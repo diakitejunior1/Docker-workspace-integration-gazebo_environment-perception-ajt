532c23076e49927178e906d1a3a29cfbb6e4c13a
Yunus Ahmet AKDAL <yunus.akdal@gmail.com>
2025-02-27 21:14:04 +0300

    Fix run.bat can not link volume to container fixed

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ rem Check if the container is already running
 for /f %%i in ('docker ps -a --quiet --filter "status=running" --filter "name=%CONTAINER_NAME%"') do set runningContainer=%%i
 if defined runningContainer (
     echo Attaching to running container: %CONTAINER_NAME%
-    docker exec -i -t %CONTAINER_NAME% cmd
+    docker exec -i -t %CONTAINER_NAME% /bin/bash
     exit /b 0
 )
 
@@ -17,7 +17,7 @@ echo Building %DOCKERFILE% as image: %IMAGE_NAME%
 docker build -f %DOCKERFILE% -t %IMAGE_NAME% .
 
 rem Define Docker arguments
-set DOCKER_ARGS=--name %CONTAINER_NAME% -v ./workspace:/home/ubuntu/workspace -p 6081:80 --security-opt seccomp=unconfined --shm-size=512m
+set DOCKER_ARGS=--name %CONTAINER_NAME% -v %cd%/workspace:/home/ubuntu/workspace/ -p 6081:80 --security-opt seccomp=unconfined --shm-size=512m
 
 rem Run the container
 docker run -it --rm %DOCKER_ARGS% %IMAGE_NAME%
